72cd127043711e0e2a82b6967d977801c2647429
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-11 19:38:23 +0300

    header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
- import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-
-import Header from './Header'
+import header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
  
