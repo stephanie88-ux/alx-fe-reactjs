880fcdd2782daccf39146be813f32cca96094fb3
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-11 19:23:41 +0300

    header components

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,8 +1,8 @@
-function header () {
+function Header() {
     return (
         <header>
         <h1>My Favorite Cities</h1> 
         </header>
     );
 }
-export default header;
+export default Header;
