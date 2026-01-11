c7a88b305b735d7c3b1930e2968f9d7ea4978635
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-11 20:03:56 +0300

    UserProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
