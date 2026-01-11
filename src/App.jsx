90bc15b0269f9c90a5cab0354893fb0ca6a77941
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-11 19:33:17 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
- import Header from './Header'
+
 import MainContent from './MainContent'
 import Footer from './Footer'
+import header from './Header'
 
   function app () {
   return (
