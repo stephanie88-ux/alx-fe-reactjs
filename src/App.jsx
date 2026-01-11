4a364c62666e25e48529efe3facae0345f50c852
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-11 19:35:26 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 
+import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
-import header from './Header'
+ 
 
   function app () {
   return (
