314f357220ebe15c87d44a0b485cc35274ac3ebb
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-20 17:27:39 +0300

    using the function counter

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import  {useState} from 'react';
+
+function counter {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+    );
+}
