a07a40224729b852ef9013ce280fa437864c53cd
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-20 16:41:58 +0300

    updated files

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
-function UserProfile = (props) => {
+function UserProfile(props) {
     return (
-        <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-        </div>
+        <><div>
+            <h2>{props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
+            </div></>
     );
-};
+}
