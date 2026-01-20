93e2f641dfb5fb111b842dc7b29d9c91bbe1878a
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-20 16:57:46 +0300

    deleted userProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+
 
 function App() {
   return (
