d3784dfeb476d112a0ddc2b865055a5017f8e73a
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-20 12:55:19 +0300

    CSS styling Haeder

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -7,6 +7,7 @@ function Header() {
                 textAlign: 'center'     
             }}>
                 <h1>My Favorite Cities</h1>
+                
             </header>
         </div>
     );
