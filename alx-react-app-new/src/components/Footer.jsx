bac140037d983218ca27dc778ca55c50a9807770
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-20 17:16:16 +0300

    update in line CSS

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer () {
+    return (
+        <>
+            <footer>
+                <p>© 2023 City Lovers</p>
+            </footer>
+            <div style={{ 
+                border: '1px solid blue', 
+                backgroundColor: 'lightgray',
+                padding: '15px', 
+                margin: '10px' }}>
+                <p>© 2023 City Lovers</p>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <main style={{ 
+      padding: '30px', 
+      backgroundColor: 'white',
+      textAlign: 'center'
+    }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
