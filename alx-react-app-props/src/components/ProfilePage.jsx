e030ccccc0af3b134d7ff14f970ad1bcf48b2e28
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-23 18:51:02 +0300

    adding usercontext to userprofile

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { UserContext } from '../context/UserContext';
+import { UserContext } from './UserContext';
 import UserInfo from './UserInfo';
 
 function ProfilePage() {
