2c4bcabbc237c6cb5d32b54b11288fee2ee396b0
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-23 21:09:04 +0300

    add navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -9,16 +9,7 @@ import Contact from "./my-company/Contact";
 function App() {
   return (
     <BrowserRouter>
-
-    <nav style ={{ padding: '10px', backgroundColor: '#f0f0f0',  marginBottom: '20px' }}>
-    <Link to="/" style ={{ color: 'white', padding: '5px' }}>Home</Link>
-    <Link to="/about" style ={{ backgroundColor: '#ccc', padding: '5px' }}>About</Link>
-    <Link to="/services" style ={{ color: 'white', padding: '5px' }}>Services</Link>
-    <Link to="/contact" style ={{ color: 'white', padding: '5px' }}>Contact</Link>
-    
-    </nav>
-
-
+    <navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
