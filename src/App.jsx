7a62954584f4b9df4093899779ad4b9e9ae431c0
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-23 20:17:36 +0300

    Building a website using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { BrowserRoute, Routes, Route } from "react-router-dom";  
+
 import Home from "./my-company/Home";
 import About from "./my-company/About";
 import services from "./my-company/Services";
 import Contact from "./my-company/Contact";
 
+
 function App() {
   return (
-    <BrowserRoute>
+    <BrowserRouter>
+
+    <nav style ={{ padding: '10px', backgroundColor: '#f0f0f0',  marginBottom: '20px' }}>
+    <Link to="/" style ={{ color: 'white', padding: '5px' }}>Home</Link>
+    <Link to="/about" style ={{ backgroundColor: '#ccc', padding: '5px' }}>About</Link>
+    <Link to="/services" style ={{ color: 'white', padding: '5px' }}>Services</Link>
+    <Link to="/contact" style ={{ color: 'white', padding: '5px' }}>Contact</Link>
+    </nav>
+
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRoute>
+
+    </BrowserRouter>
   )
 }
 
