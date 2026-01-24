03b0c3e0e93ee3acc63bdff236dd15116c94398d
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-25 02:37:43 +0300

    move recipeStore to components

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,8 @@
+import create from'Zustand'
+const UseRecipeStore = create((set) => ({
+    recipes: [],
+    addRecipe : (recipe) => set( state => ({recipe : [...state.recipes, recipe]})),
+    setRecipe : (recipes) => set({recipes}),
+}));
+
+export default UseRecipeStore;
