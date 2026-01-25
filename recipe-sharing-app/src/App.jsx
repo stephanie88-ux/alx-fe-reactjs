9f05ebc71e415fcf05ddb8e26f6373662bb1529a
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-25 19:43:48 +0300

    add path,elemement and path

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
+import RecipeDetail from './components/RecipeDetails';
+import element from 'react';
+import path from 'path';
 import { BrowserRouter, ReactDom } from 'react-router-dom';
 import Routes from './Routes';
 
