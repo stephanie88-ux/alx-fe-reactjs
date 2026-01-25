c8dcec994d3d07054010ed875a84cc88b82cf271
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-25 03:15:03 +0300

    update and delete recipeStore file

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,5 +1,4 @@
- import { useRecipeStore } from '../components/recipeStore';
-
+import { addRecipe, deleteRecipe, updateRecipe } from '../recipeDetails';
 const RecipeDetails = ({ recipe }) => {
     const recipe = useRecipeStore((state) => 
         state.recipes.find (recipe => recipe.id === recipe.id)
