28cb4f44282a5a644596a5b18f95992c38175e20
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-25 04:04:39 +0300

    updating Routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import  Path from 'path';
-import Route from './routes/Route';
+import Routes from './routes/Route';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <Route />
+        <Routes />
     </BrowserRouter>
 );
 
