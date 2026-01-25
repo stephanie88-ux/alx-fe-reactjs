babcb17c6f6cb284c0cf46733be438ccf5a19aa4
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-25 03:51:50 +0300

    use navigate

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -4,15 +4,15 @@ const RecipeDetails = ({ recipe }) => {
     const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
     const handleDelete = () => {
            deleteRecipe(recipe.id);
+           <navigate/>
 }};
 
 return (
     <div>
         <h2>{recipe.title}</h2>
         <p>{recipe.ingredients}</p>
-        <navigate>
         <button onClick={handleDelete}>Delete Recipe</button>
-        </navigate>
+        
     </div>
 );
 export default RecipeDetails;   
