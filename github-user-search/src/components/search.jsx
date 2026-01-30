b3f17c081c395cc252552c070561baeacfc5719f
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-30 20:25:53 +0300

    update Search.jsx

--- github-user-search/src/components/search.jsx
+++ github-user-search/src/components/search.jsx
@@ -3,24 +3,4 @@ import {useState} from 'react';
 function Search({onSearch}) {
   const [username, setUsername] = useState('');
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSearch(username);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter GitHub username"
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
-}
-
-export default Search;
-
-
+};
