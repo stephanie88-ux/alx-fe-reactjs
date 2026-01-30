7a41a3e56f578bf51735575c40e0e1eacbc2b69d
stephaniechepkosgei <stephieem88@gmail.com>
2026-01-30 19:33:23 +0300

    installed packages

--- recipe-sharing-app/src/stores/RecipeStore.js
+++
@@ -1,8 +0,0 @@
-import create from'Zustand'
-const UseRecipeStore = create((set) => ({
-    recipes: [],
-    addRecipe : (recipe) => set( state => ({recipe : [...state.recipes, recipe]})),
-    setRecipe : (recipes) => set({recipes}),
-}));
-
-export default UseRecipeStore;
