ad0f3038da64eb36f15ffcb89375640ba8dd6c2a
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-01 21:36:26 +0300

    update location, minrepos

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -7,5 +7,19 @@ export const fetchUserData = async (username) => {
   return response.data;
 };
 
-
+export const userFetchData = async ({username, location, minRepos}) => {
+    try {   
+    let query = `https://api.github.com/search/users?q=${username}`;
+    if (location) {
+        query += `+location:${location}`;
+    }
+    if (minRepos) {
+        query += `+repos:>=${minRepos}`;
+    }
+    const response = await axios.get(query);
+    return response.data.items;     
+} catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+}};
  
