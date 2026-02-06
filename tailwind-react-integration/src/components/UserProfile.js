c0e46e4469c6deca413e206ccdfc4b056e9ffbf1
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-07 00:09:59 +0300

    styling react component with tailwind css

---
+++ tailwind-react-integration/src/components/UserProfile.js
@@ -0,0 +1,19 @@
+function UserProfile () {
+return (
+    <div className="UserProfile">
+        <img src="https://via.placeholder.com/150" alt="User"/>
+        <h1>John Doe</h1>
+        <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
+
+        <div className="User-Profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        </div>
+        <div className="group">
+            <img src="https://via.placeholder.com/150" className="rounded-full w-36 h-36 mx-auto" alt="User"/>
+            <h1 className="text-xl text-blue-800 my-4 vertical-margin">John Doe</h1>
+            <p className="text-gray-800 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        </div>
+    </div>
+);
+}
+
+export default UserProfile;
