019ce72e3e8c72da044f4392d9127943e583aaf5
stephaniechepkosgei <stephieem88@gmail.com>
2026-02-07 00:16:16 +0300

    Add import UserProifile to App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { UserProifile } from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
